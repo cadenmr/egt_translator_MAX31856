5435a159f1b890a6ea2a43fd05611058b4473d21
cadenmr <37202505+cadenmr@users.noreply.github.com>
2023-01-07 23:12:29 -0700

    Add files via upload

--- egt_translator_MAX31856.ino
+++ egt_translator_MAX31856.ino
@@ -1,23 +1,64 @@
-#include <Adafruit_MAX31856.h>
+// -------------- CONFIGURATION SECTION --------------
 
-#define EGT_OUTPUT  9  // must be 9 or 10 for 16-bit PWM
+// Output pin, MUST be 9 or 10!
+#define EGT_OUTPUT  9
 
+// Minimum and maximum temperatures in degC. Narrowing the range increases resolution and accuracy.
+// This range MUST match the range set up in the ECU or the reading will be incorrect!
+// Valid values are -100 to 1350
 #define EGT_SENSOR_MIN_TEMP_C 0
-#define EGT_SENSOR_MAX_TEMP_C 1350
+#define EGT_SENSOR_MAX_TEMP_C 1100
 
-#define SPI_CHIPSELECT   4
-#define SPI_DRDY         8
+// Special pins for SPI. Any pins may be used.
+#define SPI_CHIPSELECT   2
+#define SPI_DRDY         3
+
+// ---------------------------------------------------
+
+#include <Adafruit_MAX31856.h>
 
 Adafruit_MAX31856 egtSensor(SPI_CHIPSELECT);  // set up sensor with hardware SPI
 
 void setup() {
+
+  Serial.begin(115200);
+
+  // check for configuration issues
+  bool configErrorDetected = false;
+  if (EGT_OUTPUT != 9 && EGT_OUTPUT != 10) { configErrorDetected = true; }  // Output pin error
+  if (EGT_SENSOR_MIN_TEMP_C > EGT_SENSOR_MAX_TEMP_C) { configErrorDetected = true; }  // Range error
+  if ((EGT_SENSOR_MIN_TEMP_C < -100 || EGT_SENSOR_MIN_TEMP_C > 1350) || 
+      (EGT_SENSOR_MAX_TEMP_C > 1350 || EGT_SENSOR_MAX_TEMP_C < -100)) { configErrorDetected = true; }  // Range out-of-bounds
+  
+  if (configErrorDetected == true) {
+    while (true) {
+      
+      if (EGT_OUTPUT != 9 && EGT_OUTPUT != 10) {
+        Serial.println("Config Fault: Incorrect Output Pin");
+        egtErrorPulse(1001);
+      }
+      
+      if (EGT_SENSOR_MIN_TEMP_C > EGT_SENSOR_MAX_TEMP_C) {
+        Serial.println("Config Fault: Min Output Range > Max Output Range");
+        egtErrorPulse(1001);
+      }
+
+      if ((EGT_SENSOR_MIN_TEMP_C < -100 || EGT_SENSOR_MIN_TEMP_C > 1350) || 
+      (EGT_SENSOR_MAX_TEMP_C > 1350 || EGT_SENSOR_MAX_TEMP_C < -100)) {
+        Serial.println("Config Fault: Output Range Out Of Bounds");
+        egtErrorPulse(1001);
+      }
+    }
+  }
+  
   setupPWM16();  // set up 16 bit pwm
   pinMode(SPI_DRDY, INPUT);
 
   // attempt to init thermocouple
   if (!egtSensor.begin()) {
     while (true) {
-      egtErrorPulse(100);  // Thermocouple Init Fault 
+      egtErrorPulse(901);
+      Serial.println("Thermocouple Init Fault");
     }
   }
 
@@ -34,15 +75,15 @@ void loop() {
   // check for faults
   uint8_t fault = egtSensor.readFault();
   if (fault) {
-    if (fault & MAX31856_FAULT_CJRANGE) egtErrorPulse(10);  // Cold Junction Range Fault
-    if (fault & MAX31856_FAULT_TCRANGE) egtErrorPulse(20);  // Thermocouple Range Fault
-    if (fault & MAX31856_FAULT_CJHIGH)  egtErrorPulse(30);  // Cold Junction High Fault
-    if (fault & MAX31856_FAULT_CJLOW)   egtErrorPulse(40);  // Cold Junction Low Fault
-    if (fault & MAX31856_FAULT_TCHIGH)  egtErrorPulse(50);  // Thermocouple High Fault
-    if (fault & MAX31856_FAULT_TCLOW)   egtErrorPulse(60);  // Thermocouple Low Fault
-    if (fault & MAX31856_FAULT_OVUV)    egtErrorPulse(70);  // Over/Under Voltage Fault
-    if (fault & MAX31856_FAULT_OPEN)    egtErrorPulse(80);  // Thermocouple Open Fault
-  } else { setEgtOutput(egtDegC); }  // no fault. set as normal.
+    if (fault & MAX31856_FAULT_CJRANGE) {egtErrorPulse(101); Serial.println("Cold Junction Range Fault");}
+    if (fault & MAX31856_FAULT_TCRANGE) {egtErrorPulse(201); Serial.println("Thermocouple Range Fault");}
+    if (fault & MAX31856_FAULT_CJHIGH)  {egtErrorPulse(301); Serial.println("Cold Junction High Fault");}
+    if (fault & MAX31856_FAULT_CJLOW)   {egtErrorPulse(401); Serial.println("Cold Junction Low Fault");}
+    if (fault & MAX31856_FAULT_TCHIGH)  {egtErrorPulse(501); Serial.println("Thermocouple High Fault");}
+    if (fault & MAX31856_FAULT_TCLOW)   {egtErrorPulse(601); Serial.println("Thermocouple Low Fault");}
+    if (fault & MAX31856_FAULT_OVUV)    {egtErrorPulse(701); Serial.println("Over/Under Voltage Fault");}
+    if (fault & MAX31856_FAULT_OPEN)    {egtErrorPulse(801); Serial.println("Thermocouple Open Fault");}
+  } else { setEgtOutput(egtDegC); Serial.println(egtDegC); }  // no fault. set as normal.
 }
 
 void setEgtOutput(double degreesC) {
