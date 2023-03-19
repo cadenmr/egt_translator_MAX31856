173195ddb58c0352a23f008311447119bb06d3ec
cadenmr <37202505+cadenmr@users.noreply.github.com>
2023-03-18 22:58:19 -0700

    update

--- egt_translator_MAX31856.ino
+++ egt_translator_MAX31856.ino
@@ -1,7 +1,8 @@
 // -------------- CONFIGURATION SECTION --------------
 
 // Output pin, MUST be 9 or 10!
-#define EGT_OUTPUT  9
+#define EGT_OUTPUT_SENSOR1  9
+#define EGT_OUTPUT_SENSOR2  10
 
 // Minimum and maximum temperatures in degC. Narrowing the range increases resolution and accuracy.
 // This range MUST match the range set up in the ECU or the reading will be incorrect!
@@ -10,83 +11,131 @@
 #define EGT_SENSOR_MAX_TEMP_C 1100
 
 // Special pins for SPI. Any pins may be used.
-#define SPI_CHIPSELECT   2
-#define SPI_DRDY         3
+#define SPI_CHIPSELECT_SENSOR1   2
+#define SPI_DRDY_SENSOR1         3
+#define SPI_CHIPSELECT_SENSOR2  4
+#define SPI_DRDY_SENSOR2        5
 
 // ---------------------------------------------------
 
 #include <Adafruit_MAX31856.h>
 
-Adafruit_MAX31856 egtSensor(SPI_CHIPSELECT);  // set up sensor with hardware SPI
+Adafruit_MAX31856 egtSensor1(SPI_CHIPSELECT_SENSOR1);  // set up sensor1 with hardware SPI
+Adafruit_MAX31856 egtSensor2(SPI_CHIPSELECT_SENSOR2);  // set up sensor2 with hardware SPI
 
 void setup() {
 
   Serial.begin(115200);
 
-  // check for configuration issues
-  bool configErrorDetected = false;
-  if (EGT_OUTPUT != 9 && EGT_OUTPUT != 10) { configErrorDetected = true; }  // Output pin error
-  if (EGT_SENSOR_MIN_TEMP_C > EGT_SENSOR_MAX_TEMP_C) { configErrorDetected = true; }  // Range error
-  if ((EGT_SENSOR_MIN_TEMP_C < -100 || EGT_SENSOR_MIN_TEMP_C > 1350) || 
-      (EGT_SENSOR_MAX_TEMP_C > 1350 || EGT_SENSOR_MAX_TEMP_C < -100)) { configErrorDetected = true; }  // Range out-of-bounds
+  // TODO: Reimplement this for 2x sensors
   
-  if (configErrorDetected == true) {
-    while (true) {
+  // check for configuration issues
+  // bool configErrorDetected = false;
+  // if (EGT_OUTPUT_SENSOR1 != 9 && EGT_OUTPUT_SENSOR1 != 10) { configErrorDetected = true; }  // Output pin error
+  // if (EGT_SENSOR_MIN_TEMP_C > EGT_SENSOR_MAX_TEMP_C) { configErrorDetected = true; }  // Range error
+  // if ((EGT_SENSOR_MIN_TEMP_C < -100 || EGT_SENSOR_MIN_TEMP_C > 1350) || 
+  //     (EGT_SENSOR_MAX_TEMP_C > 1350 || EGT_SENSOR_MAX_TEMP_C < -100)) { configErrorDetected = true; }  // Range out-of-bounds
+  
+  // if (configErrorDetected == true) {
+  //   while (true) {
       
-      if (EGT_OUTPUT != 9 && EGT_OUTPUT != 10) {
-        Serial.println("Config Fault: Incorrect Output Pin");
-        egtErrorPulse(1001);
-      }
+  //     if (EGT_OUTPUT_SENSOR1 != 9 && EGT_OUTPUT_SENSOR1 != 10) {
+  //       Serial.println("Config Fault: Incorrect Output Pin");
+  //       egtErrorPulse1(1001);
+  //     }
       
-      if (EGT_SENSOR_MIN_TEMP_C > EGT_SENSOR_MAX_TEMP_C) {
-        Serial.println("Config Fault: Min Output Range > Max Output Range");
-        egtErrorPulse(1001);
-      }
+  //     if (EGT_SENSOR_MIN_TEMP_C > EGT_SENSOR_MAX_TEMP_C) {
+  //       Serial.println("Config Fault: Min Output Range > Max Output Range");
+  //       egtErrorPulse1(1001);
+  //     }
 
-      if ((EGT_SENSOR_MIN_TEMP_C < -100 || EGT_SENSOR_MIN_TEMP_C > 1350) || 
-      (EGT_SENSOR_MAX_TEMP_C > 1350 || EGT_SENSOR_MAX_TEMP_C < -100)) {
-        Serial.println("Config Fault: Output Range Out Of Bounds");
-        egtErrorPulse(1001);
-      }
-    }
-  }
+  //     if ((EGT_SENSOR_MIN_TEMP_C < -100 || EGT_SENSOR_MIN_TEMP_C > 1350) || 
+  //     (EGT_SENSOR_MAX_TEMP_C > 1350 || EGT_SENSOR_MAX_TEMP_C < -100)) {
+  //       Serial.println("Config Fault: Output Range Out Of Bounds");
+  //       egtErrorPulse1(1001);
+  //     }
+  //   }
+  // }
   
   setupPWM16();  // set up 16 bit pwm
-  pinMode(SPI_DRDY, INPUT);
+  pinMode(SPI_DRDY_SENSOR1, INPUT);
+  pinMode(SPI_DRDY_SENSOR2, INPUT);
 
-  // attempt to init thermocouple
-  if (!egtSensor.begin()) {
+  // attempt to init thermocouple 1
+  if (!egtSensor1.begin()) {
     while (true) {
-      egtErrorPulse(901);
-      Serial.println("Thermocouple Init Fault");
+      egtErrorPulse1(901);
+      Serial.println("Thermocouple 1 Init Fault");
     }
   }
 
-  egtSensor.setThermocoupleType(MAX31856_TCTYPE_K);  // set type K thermocouple
-  egtSensor.setConversionMode(MAX31856_CONTINUOUS);  // set continuous sampling mode
+// attempt to init thermocouple 2
+  if (!egtSensor2.begin()) {
+    while (true) {
+      egtErrorPulse1(901);
+      Serial.println("Thermocouple 2 Init Fault");
+    }
+  }
+
+  egtSensor1.setThermocoupleType(MAX31856_TCTYPE_K);  // set type K thermocouple
+  egtSensor1.setConversionMode(MAX31856_CONTINUOUS);  // set continuous sampling mode
+
+  egtSensor2.setThermocoupleType(MAX31856_TCTYPE_K);  // set type K thermocouple
+  egtSensor2.setConversionMode(MAX31856_CONTINUOUS);  // set continuous sampling mode
+  
 }
 
 void loop() {
 
-  while (digitalRead(SPI_DRDY)) { ; }  // do nothing while waiting for new data
-  
-  double egtDegC = egtSensor.readThermocoupleTemperature();  // read temp from sensor in degC
+  // while (digitalRead(SPI_DRDY_SENSOR1)) { ; }  // do nothing while waiting for new data
 
-  // check for faults
-  uint8_t fault = egtSensor.readFault();
-  if (fault) {
-    if (fault & MAX31856_FAULT_CJRANGE) {egtErrorPulse(101); Serial.println("Cold Junction Range Fault");}
-    if (fault & MAX31856_FAULT_TCRANGE) {egtErrorPulse(201); Serial.println("Thermocouple Range Fault");}
-    if (fault & MAX31856_FAULT_CJHIGH)  {egtErrorPulse(301); Serial.println("Cold Junction High Fault");}
-    if (fault & MAX31856_FAULT_CJLOW)   {egtErrorPulse(401); Serial.println("Cold Junction Low Fault");}
-    if (fault & MAX31856_FAULT_TCHIGH)  {egtErrorPulse(501); Serial.println("Thermocouple High Fault");}
-    if (fault & MAX31856_FAULT_TCLOW)   {egtErrorPulse(601); Serial.println("Thermocouple Low Fault");}
-    if (fault & MAX31856_FAULT_OVUV)    {egtErrorPulse(701); Serial.println("Over/Under Voltage Fault");}
-    if (fault & MAX31856_FAULT_OPEN)    {egtErrorPulse(801); Serial.println("Thermocouple Open Fault");}
-  } else { setEgtOutput(egtDegC); Serial.println(egtDegC); }  // no fault. set as normal.
+  while (true) {  // reading loop, check if we have new data from either sensor and set it
+
+    if (!digitalRead(SPI_DRDY_SENSOR1)) {   // if we have data ready for sensor 1
+
+      double egtDegC_sensor1 = egtSensor1.readThermocoupleTemperature();  // read temp from sensor in degC
+
+      // check for faults
+      uint8_t fault = egtSensor1.readFault();
+      if (fault) {
+        if (fault & MAX31856_FAULT_CJRANGE) {setEgtOutput1(101); Serial.println("1: Cold Junction Range Fault");}
+        if (fault & MAX31856_FAULT_TCRANGE) {setEgtOutput1(201); Serial.println("1: Thermocouple Range Fault");}
+        if (fault & MAX31856_FAULT_CJHIGH)  {setEgtOutput1(301); Serial.println("1: Cold Junction High Fault");}
+        if (fault & MAX31856_FAULT_CJLOW)   {setEgtOutput1(401); Serial.println("1: Cold Junction Low Fault");}
+        if (fault & MAX31856_FAULT_TCHIGH)  {setEgtOutput1(501); Serial.println("1: Thermocouple High Fault");}
+        if (fault & MAX31856_FAULT_TCLOW)   {setEgtOutput1(601); Serial.println("1: Thermocouple Low Fault");}
+        if (fault & MAX31856_FAULT_OVUV)    {setEgtOutput1(701); Serial.println("1: Over/Under Voltage Fault");}
+        if (fault & MAX31856_FAULT_OPEN)    {setEgtOutput1(801); Serial.println("1: Thermocouple Open Fault");}
+      } else { setEgtOutput1(egtDegC_sensor1); Serial.print("1: "); Serial.println(egtDegC_sensor1); }  // no fault. set as normal.
+
+    }
+    
+    if (!digitalRead(SPI_DRDY_SENSOR2)) {   // if we have data ready for sensor 2
+
+      double egtDegC_sensor2 = egtSensor2.readThermocoupleTemperature();  // read temp from sensor in degC
+
+      // check for faults
+      uint8_t fault = egtSensor2.readFault();
+      if (fault) {
+        if (fault & MAX31856_FAULT_CJRANGE) {setEgtOutput2(101); Serial.println("2: Cold Junction Range Fault");}
+        if (fault & MAX31856_FAULT_TCRANGE) {setEgtOutput2(201); Serial.println("2: Thermocouple Range Fault");}
+        if (fault & MAX31856_FAULT_CJHIGH)  {setEgtOutput2(301); Serial.println("2: Cold Junction High Fault");}
+        if (fault & MAX31856_FAULT_CJLOW)   {setEgtOutput2(401); Serial.println("2: Cold Junction Low Fault");}
+        if (fault & MAX31856_FAULT_TCHIGH)  {setEgtOutput2(501); Serial.println("2: Thermocouple High Fault");}
+        if (fault & MAX31856_FAULT_TCLOW)   {setEgtOutput2(601); Serial.println("2: Thermocouple Low Fault");}
+        if (fault & MAX31856_FAULT_OVUV)    {setEgtOutput2(701); Serial.println("2: Over/Under Voltage Fault");}
+        if (fault & MAX31856_FAULT_OPEN)    {setEgtOutput2(801); Serial.println("2: Thermocouple Open Fault");}
+      } else { setEgtOutput2(egtDegC_sensor2); Serial.print("2: "); Serial.println(egtDegC_sensor2); }  // no fault. set as normal.
+
+    } 
+
+  }
+
 }
 
-void setEgtOutput(double degreesC) {
+// TODO: USE INTERRUPTS FOR THIS INSTEAD
+
+void setEgtOutput1(double degreesC) {
 
   // temperature limits
   if (degreesC < EGT_SENSOR_MIN_TEMP_C) {
@@ -97,20 +146,44 @@ void setEgtOutput(double degreesC) {
     degreesC = EGT_SENSOR_MAX_TEMP_C;
   }
   
-  analogWrite16(EGT_OUTPUT, map(degreesC, EGT_SENSOR_MIN_TEMP_C, EGT_SENSOR_MAX_TEMP_C, 0, 65535));
+  analogWrite16(EGT_OUTPUT_SENSOR1, map(degreesC, EGT_SENSOR_MIN_TEMP_C, EGT_SENSOR_MAX_TEMP_C, 0, 65535));
+}
+
+void setEgtOutput2(double degreesC) {
+
+  // temperature limits
+  if (degreesC < EGT_SENSOR_MIN_TEMP_C) {
+    degreesC = EGT_SENSOR_MIN_TEMP_C;
+  }
+
+  if (degreesC > EGT_SENSOR_MAX_TEMP_C) {
+    degreesC = EGT_SENSOR_MAX_TEMP_C;
+  }
+  
+  analogWrite16(EGT_OUTPUT_SENSOR2, map(degreesC, EGT_SENSOR_MIN_TEMP_C, EGT_SENSOR_MAX_TEMP_C, 0, 65535));
 }
 
 // pulsing routine used for fault diagnosis
 // half-second at 0 degrees, then 2 seconds of fault code
-// repeated 3 times
-void egtErrorPulse(double degreesC) {
+// repeated 3 times 
+void egtErrorPulse1(double degreesC) {
   for (int i = 0; i < 3; i++) {
-    setEgtOutput(0);
+    setEgtOutput1(0);
     delay(500);
-    setEgtOutput(degreesC);
+    setEgtOutput1(degreesC);
     delay(2000);
   }
-  setEgtOutput(0);
+  setEgtOutput1(0);
+}
+
+void egtErrorPulse2(double degreesC) {
+  for (int i = 0; i < 3; i++) {
+    setEgtOutput2(0);
+    delay(500);
+    setEgtOutput2(degreesC);
+    delay(2000);
+  }
+  setEgtOutput2(0);
 }
 
 void setupPWM16() {
